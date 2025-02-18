8a3314ab91ac30aef4c688368d3709210eed6c3e
AntØn UstinØff <a.a.ustinoff@gmail.com>
2025-02-18 15:30:37 +0400

    chore: refactoring

--- example/android/app/src/main/java/flutter/plugins/contactos/example/MainActivity.java
+++ example/android/app/src/main/java/flutter/plugins/contactos/example/MainActivity.java
@@ -2,5 +2,4 @@ package flutter.plugins.contactos.example;
 
 import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
-}
+public class MainActivity extends FlutterActivity {}
