b2a7fa101f4af2d21212635d567b05c700626b26
AntØn UstinØff <a.a.ustinoff@gmail.com>
2025-04-25 10:56:28 +0400

    chore: update

--- contactos_platform_interface/lib/src/method_channel_contactos.dart
+++ contactos_platform_interface/lib/src/method_channel_contactos.dart
@@ -1,6 +1,6 @@
-// Copyright 2025 The Anton Ustinoff <a.a.ustinoff@gmail.com>.
-// All rights reserved. Use of this source code is
-// governed by a MIT-style license that can be found in the LICENSE file.
+// Copyright 2025 Anton Ustinoff<a.a.ustinoff@gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'dart:async';
 
