5636dd272b54a7ea74fb44b1004f4394884ec344
AntØn UstinØff <a.a.ustinoff@gmail.com>
2025-11-25 18:14:26 +0400

    feat: add `android` implementation

---
+++ contactos_android/example/README.md
@@ -0,0 +1,8 @@
+# contactos_example
+
+Demonstrates how to use the contactos plugin.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ contactos_android/example/lib/src/screens/navite_contacts_picker_screen.dart
@@ -0,0 +1,62 @@
+/*
+ * Author: Anton Ustinoff <https://github.com/ziqq> | <a.a.ustinoff@gmail.com>
+ * Date: 25 November 2025
+ */
+
+import 'dart:developer';
+
+import 'package:contactos_android/contactos_android.dart';
+import 'package:contactos_example/main.dart' show kAndroidLocalizedLabels;
+import 'package:flutter/material.dart';
+
+/// {@template navite_contacts_picker_screen}
+/// NativeContactsPickerScreen widget.
+/// {@endtemplate}
+class NativeContactsPickerScreen extends StatefulWidget {
+  /// {@macro navite_contacts_picker_screen}
+  const NativeContactsPickerScreen({super.key});
+
+  @override
+  State<NativeContactsPickerScreen> createState() =>
+      _NativeContactsPickerScreenState();
+}
+
+/// State for widget [NativeContactsPickerScreen].
+class _NativeContactsPickerScreenState
+    extends State<NativeContactsPickerScreen> {
+  Contact? _contact;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _pickContact() async {
+    try {
+      final contact = await ContactosPluginAndroid.instance
+          .openDeviceContactPicker(
+            androidLocalizedLabels: kAndroidLocalizedLabels,
+          );
+      setState(() => _contact = contact);
+    } on Object catch (e) {
+      log(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: const Text('Contacts Picker Example')),
+    body: SafeArea(
+      child: Column(
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: _pickContact,
+            child: const Text('Pick a contact'),
+          ),
+          if (_contact != null)
+            Text('Contact selected: ${_contact?.displayName}'),
+        ],
+      ),
+    ),
+  );
+}
